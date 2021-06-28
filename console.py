b7fd2446fd62a0e57a365d83c6ad881a89210462
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-28 20:10:15 +0000

    added user class and updated console (task 8)

--- console.py
+++ console.py
@@ -4,7 +4,11 @@
 import cmd
 import models
 from models.base_model import BaseModel
-import models
+from models.user import User
+
+white_list = ["BaseModel", "User"]
+class_list = [BaseModel(), User()]
+
 
 class HBNBCommand(cmd.Cmd):
     """Representation of a HBNBCommand"""
@@ -24,65 +28,83 @@ class HBNBCommand(cmd.Cmd):
         args = line.split()
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in white_list:
             print("** class doesn't exist **")
         else:
-            new_instance = BaseModel()
+            for i in range(white_list):
+                if args[i] == white_list[i]:
+                    return
+            new_instance = class_list[i]
             print(new_instance.id)
             new_instance.save()
-    
+
     def do_show(self, line):
+        """prints the string representation of an instance
+           based on the class name and id"""
         args = line.split()
+        objects_dic = models.storage.all()
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in white_list:
             print("** class doesn't exist **")
         elif len(args) != 2:
             print("** instance id missing **")
-        elif "BaseModel."+args[1] in models.storage.all():
-            objects_dic = models.storage.all()
-            print(objects_dic["BaseModel."+args[1]])
+        elif args[0]+"."+args[1] in objects_dic:
+            print(objects_dic[args[0]+"."+args[1]])
         else:
             print("** no instance found **")
 
     def do_destroy(self, line):
+        """deletes an instance based on the class name and id"""
         args = line.split()
+        objects_dic = models.storage.all()
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in white_list:
             print("** class doesn't exist **")
         elif len(args) != 2:
             print("** instance id missing **")
-        elif "BaseModel."+args[1] in models.storage.all():
-            models.storage.all().pop("BaseModel."+args[1])
+        elif args[0]+"."+args[1] in objects_dic:
+            models.storage.all().pop(args[0]+"."+args[1])
         else:
             print("** no instance found **")
 
     def do_all(self, line):
+        """prints all string representation of all instances
+           based (or not) on the class name"""
         args = line.split()
-        if len(args) == 0 or args[0] == "BaseModel":
-            objects_dic = models.storage.all()
+        objects_dic = models.storage.all()
+        if len(args) == 0:
             for key in objects_dic:
                 print(objects_dic[key])
+        elif args[0] in white_list:
+            for key in objects_dic:
+                if objects_dic[key].__class__.__name__ == args[0]:
+                    print(objects_dic[key])
         else:
             print("** class doesn't exist **")
 
     def do_update(self, line):
+        """updates an instance based on the class name and id
+           by adding or updating attribute"""
         args = line.split()
+        objects_dic = models.storage.all()
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in white_list:
             print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
-        elif not ("BaseModel."+args[1] in models.storage.all()):
+        elif args[0]+"."+args[1] not in objects_dic:
             print("** no instance found **")
         elif len(args) < 3:
             print("** attribute name missing **")
         elif len(args) < 4:
             print("** value missing **")
         else:
-            setattr(models.storage.all()["BaseModel."+args[1]], args[2], args[3])
+            setattr(models.storage.all()[args[0]+"."+args[1]],
+                    args[2], args[3])
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""User class"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
