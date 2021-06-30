1fef2d9f4b8e00c1f077443d35141e9f1d4c2e05
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-30 21:50:25 +0000

    testing

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import shlex
 
 class_list = {"BaseModel": BaseModel,
               "User": User,
@@ -105,9 +106,45 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-    def do_update(self, line):
+    def do_update(self, arg):
         """updates an instance based on the class name and id
            by adding or updating attribute"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in class_list:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            if args[0] == "Place":
+                                if args[2] in integers:
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif args[2] in floats:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(storage.all()[k], args[2], args[3])
+                            storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+        """
         args = line.split()
         objects_dic = storage.all()
         if len(args) == 0:
@@ -136,7 +173,7 @@ class HBNBCommand(cmd.Cmd):
                     value = float(value)
             setattr(storage.all()[key], attr, value)
         storage.all()[key].save()
-
+        """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
