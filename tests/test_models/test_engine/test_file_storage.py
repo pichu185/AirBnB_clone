67854670250bf4e691f2ed3302815c91d5333ed2
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-07-01 02:29:07 +0000

    updated test cases for class FileStorage (task 5)

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@
 import models
 import datetime
 import unittest
+import os
 
 
 class FileStorageTest(unittest.TestCase):
@@ -19,10 +20,47 @@ class FileStorageTest(unittest.TestCase):
         self.assertIsNotNone(shortcut.new.__doc__)
         self.assertIsNotNone(shortcut.save.__doc__)
         self.assertIsNotNone(shortcut.reload.__doc__)
+
     def test_class(self):
         """test instance class"""
         instance = models.engine.file_storage.FileStorage()
         self.assertIsInstance(instance, models.engine.file_storage.FileStorage)
+
+    def test_all(self):
+        """test all method"""
+        instance = models.engine.file_storage.FileStorage()
+        dictionary = instance.all()
+        self.assertIsNotNone(dictionary)
+        self.assertIsInstance(dictionary, dict)
+        self.assertIs(dictionary, instance._FileStorage__objects)
+
+    def test_new(self):
+        """test new method"""
+        dict1 = models.storage.all().copy()
+        instance = models.base_model.BaseModel()
+        dict2 = models.storage.all().copy()
+        self.assertLess(len(dict1), len(dict2))
+
+    def test_save(self):
+        """test save method"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+        self.assertFalse(os.path.exists("file.json"))
+        instance = models.base_model.BaseModel()
+        instance.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload(self):
+        """test reload method"""
+        instance = models.engine.file_storage.FileStorage()
+        dic1 = models.storage.all().copy()
+        models.storage.reload()
+        dic2 = models.storage.all().copy()
+        self.assertEqual(len(dic1), len(dic2))
+        instance.save()
+        self.assertIsInstance(instance._FileStorage__file_path, str)
+        self.assertIsInstance(instance._FileStorage__objects, dict)
+        self.assertTrue(os.path.exists("file.json"))
 
 
 if __name__ == "__main__":
